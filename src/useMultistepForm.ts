203ab37f589eb964dc3be5d65cd861983285028a
Nosferatu <davonb113@gmail.com>
2022-10-12 13:10:32 -0500

    useMultistepForm hook setup

---
+++ src/useMultistepForm.ts
@@ -0,0 +1,31 @@
+import { ReactElement, useState } from "react";
+
+export function useMultistepForm(steps: ReactElement[]) {
+    const [currentStepIndex, setCurrentStepIndex] = useState(0)
+
+    function next() {
+        setCurrentStepIndex((index: number) => {
+            if(index >= steps.length - 1) return index
+            return index + 1
+        })
+    }
+
+    function back() {
+        setCurrentStepIndex((index: number) => {
+            if(index <= 0) return index
+            return index -1 
+        })
+    }
+
+    function goTo(index: number) {
+        setCurrentStepIndex(index)
+    }
+
+    return {
+        currentStepIndex,
+        step: steps[currentStepIndex],
+        goTo,
+        next,
+        back
+    }
+}
